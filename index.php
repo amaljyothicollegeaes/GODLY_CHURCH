c529f21f96493ed06b1549cbab9089a4889efaa1
ajulkjose246 <mail.ajulkjose@gmail.com>
2023-02-04 19:16:31 +0530

    commit from windows

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GODLY CHURCH</title>
+  <!-- css -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg bg_color">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">GODLY CHURCH</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Contacts
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Settings
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- end-navbar -->
+  <section class="hero">
+
+    <div class="container col-xxl-8 px-4 py-5">
+      <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-12 col-lg-6">
+          <img src="assets/img/preyer.jpeg" class="d-block preyer_img mx-lg-auto img-fluid" width="700" height="500" loading="lazy">
+        </div>
+        <div class="col-lg-6">
+          <h1 class="display-5 fw-bold lh-1 mb-3 text-light">St. George Church Adakkakundu</h1>
+          <p class="lead text-light"><b>' God bestows His blessings without discrimination. The followers of Jesus are children of God, and they
+              should manifest the family likeness by doing good to all, even to those who deserve the opposite. '</b></p>
+          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+            <button type="button" class="btn_login">Login now</button>
+            <a href="https://www.youtube.com/@st.georgechurchadakkakundu2185" class="btn-live-video d-flex align-items-center" target="_blank">
+              <i class="bi bi-play-circle"></i><span>Watch Live Streaming</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="timing">
+    <div class="row">
+      <div class="col-12 section_header">
+        <h2>Holy Qurbana(Holy Mass) And Novena Schedule</h2>
+      </div>
+      <div class="col-12 col-md-6">
+        <img src="./assets/img/ihs.png" class="d-block mx-auto" alt="">
+      </div>
+      <div class="col-6 col-md-3 time_mgt">
+        <ul>
+          <li><i class="bi bi-calendar-event"></i> Sunday
+            <ul>
+              <li>Novena : 06:30 Am</li>
+              <li>Holy Mass : 07:00 Am</li>
+            </ul>
+          </li>
+          <li><i class="bi bi-calendar-event"></i> Sunday
+            <ul>
+              <li>Novena : 06:30 Am</li>
+              <li>Holy Mass : 07:00 Am</li>
+            </ul>
+          </li>
+          <li><i class="bi bi-calendar-event"></i> Sunday
+            <ul>
+              <li>Novena : 06:30 Am</li>
+              <li>Holy Mass : 07:00 Am</li>
+            </ul>
+          </li>
+          <li><i class="bi bi-calendar-event"></i> Sunday
+            <ul>
+              <li>Novena : 06:30 Am</li>
+              <li>Holy Mass : 07:00 Am</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="col-6 col-md-3 time_mgt">
+        <ul>
+          <li><i class="bi bi-calendar-event"></i> Sunday
+            <ul>
+              <li>Novena : 06:30 Am</li>
+              <li>Holy Mass : 07:00 Am</li>
+            </ul>
+          </li>
+          <li><i class="bi bi-calendar-event"></i> Sunday
+            <ul>
+              <li>Novena : 06:30 Am</li>
+              <li>Holy Mass : 07:00 Am</li>
+            </ul>
+          </li>
+          <li><i class="bi bi-calendar-event"></i> Sunday
+            <ul>
+              <li>Novena : 06:30 Am</li>
+              <li>Holy Mass : 07:00 Am</li>
+            </ul>
+          </li>
+          <li><i class="bi bi-calendar-event"></i> Sunday
+            <ul>
+              <li>Novena : 06:30 Am</li>
+              <li>Holy Mass : 07:00 Am</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</body>
+<!-- script -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+</html>
