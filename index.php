230ca58892860a0785c4a911799c715ce3b17df3
ajulkjose246 <mail.ajulkjose@gmail.com>
2023-02-04 22:19:15 +0530

    change template

--- index.php
+++ index.php
@@ -2,136 +2,171 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>GODLY CHURCH</title>
-  <!-- css -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-
-  <link rel="stylesheet" href="assets/css/style.css">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>GODLY CHURCH</title>
+    <!-- css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
-<body>
-  <!-- navbar -->
-  <nav class="navbar navbar-expand-lg bg_color">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">GODLY CHURCH</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              About
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Contacts
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Settings
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- end-navbar -->
-  <section class="hero">
-
-    <div class="container col-xxl-8 px-4 py-5">
-      <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-        <div class="col-12 col-lg-6">
-          <img src="assets/img/preyer.jpeg" class="d-block preyer_img mx-lg-auto img-fluid" width="700" height="500" loading="lazy">
+<body id="page-top">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#page-top">GODLY CHURCH</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#about">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#services">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">Holy Mass</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">History</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contact Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Gallery</a></li>
+                </ul>
+            </div>
         </div>
-        <div class="col-lg-6">
-          <h1 class="display-5 fw-bold lh-1 mb-3 text-light">St. George Church Adakkakundu</h1>
-          <p class="lead text-light"><b>' God bestows His blessings without discrimination. The followers of Jesus are children of God, and they
-              should manifest the family likeness by doing good to all, even to those who deserve the opposite. '</b></p>
-          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-            <button type="button" class="btn_login">Login now</button>
-            <a href="https://www.youtube.com/@st.georgechurchadakkakundu2185" class="btn-live-video d-flex align-items-center" target="_blank">
-              <i class="bi bi-play-circle"></i><span>Watch Live Streaming</span>
-            </a>
-          </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead">
+        <div class="container px-4 px-lg-5 h-100">
+            <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                <div class="col-lg-8 align-self-end">
+                    <h1 class="text-white font-weight-bold">St. George Church Adakkakundu</h1>
+                    <hr class="divider" />
+                </div>
+                <div class="col-lg-8 align-self-baseline">
+                    <h3 class="text-white-75 mb-5">Jesus answered, “I am the way and the truth and the life. No one comes to the Father except through me<br> <b>John 14:6</b></h3>
+                    <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
+                </div>
+            </div>
         </div>
+    </header>    
+    <!-- Services-->
+    <section class="page-section" id="services">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-lg-3 col-md-6 text-center services_card">
+                    <div class="mt-5">
+                        <div class="mb-2"><img class="service_icon" src="./assets/img/live-stream.png"></div>
+                        <h3 class="h4 mb-2">Live TV</h3>
+                        <p class="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center services_card">
+                    <div class="mt-5">
+                        <div class="mb-2"><img class="service_icon" src="./assets/img/holy-chalice.png"></div>
+                        <h3 class="h4 mb-2">Holy Mass</h3>
+                        <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center services_card">
+                    <div class="mt-5">
+                        <div class="mb-2"><img class="service_icon" src="./assets/img/prayer.png"></div>
+                        <h3 class="h4 mb-2">Ready to Publish</h3>
+                        <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center services_card">
+                    <div class="mt-5">
+                        <div class="mb-2"><img class="service_icon" src="./assets/img/give.png"></div>
+                        <h3 class="h4 mb-2">Made with Love</h3>
+                        <p class="text-muted mb-0">Is it really open source if it's not made with love?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>    
+    <section>
+    <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./assets/img/bg_church.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
-  </section>
-  <section class="timing">
-    <div class="row">
-      <div class="col-12 section_header">
-        <h2>Holy Qurbana(Holy Mass) And Novena Schedule</h2>
-      </div>
-      <div class="col-12 col-md-6">
-        <img src="./assets/img/ihs.png" class="d-block mx-auto" alt="">
-      </div>
-      <div class="col-6 col-md-3 time_mgt">
-        <ul>
-          <li><i class="bi bi-calendar-event"></i> Sunday
-            <ul>
-              <li>Novena : 06:30 Am</li>
-              <li>Holy Mass : 07:00 Am</li>
-            </ul>
-          </li>
-          <li><i class="bi bi-calendar-event"></i> Sunday
-            <ul>
-              <li>Novena : 06:30 Am</li>
-              <li>Holy Mass : 07:00 Am</li>
-            </ul>
-          </li>
-          <li><i class="bi bi-calendar-event"></i> Sunday
-            <ul>
-              <li>Novena : 06:30 Am</li>
-              <li>Holy Mass : 07:00 Am</li>
-            </ul>
-          </li>
-          <li><i class="bi bi-calendar-event"></i> Sunday
-            <ul>
-              <li>Novena : 06:30 Am</li>
-              <li>Holy Mass : 07:00 Am</li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <div class="col-6 col-md-3 time_mgt">
-        <ul>
-          <li><i class="bi bi-calendar-event"></i> Sunday
-            <ul>
-              <li>Novena : 06:30 Am</li>
-              <li>Holy Mass : 07:00 Am</li>
-            </ul>
-          </li>
-          <li><i class="bi bi-calendar-event"></i> Sunday
-            <ul>
-              <li>Novena : 06:30 Am</li>
-              <li>Holy Mass : 07:00 Am</li>
-            </ul>
-          </li>
-          <li><i class="bi bi-calendar-event"></i> Sunday
-            <ul>
-              <li>Novena : 06:30 Am</li>
-              <li>Holy Mass : 07:00 Am</li>
-            </ul>
-          </li>
-          <li><i class="bi bi-calendar-event"></i> Sunday
-            <ul>
-              <li>Novena : 06:30 Am</li>
-              <li>Holy Mass : 07:00 Am</li>
-            </ul>
-          </li>
-        </ul>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
-  </section>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </section>
+    <section class="page-section bg-primary" id="about">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-lg-8 text-center">
+                    <h2 class="text-white mt-0">We've got what you need!</h2>
+                    <hr class="divider divider-light" />
+                    <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and
+                        running in no time! Choose one of our open source, free to download, and easy to use themes! No
+                        strings attached!</p>
+                    <a class="btn btn-light btn-xl" href="#services">Get Started!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="bg-light py-5">
+        <div class="container px-4 px-lg-5">
+            <div class="small text-center text-muted">Copyright &copy; 2023 - GODLY CHURCH</div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+    window.addEventListener('DOMContentLoaded', event => {
+        var navbarShrink = function() {
+            const navbarCollapsible = document.body.querySelector('#mainNav');
+            if (!navbarCollapsible) {
+                return;
+            }
+            if (window.scrollY === 0) {
+                navbarCollapsible.classList.remove('navbar-shrink')
+            } else {
+                navbarCollapsible.classList.add('navbar-shrink')
+            }
+        };
+        navbarShrink();
+        document.addEventListener('scroll', navbarShrink);
+        const mainNav = document.body.querySelector('#mainNav');
+        if (mainNav) {
+            new bootstrap.ScrollSpy(document.body, {
+                target: '#mainNav',
+                offset: 74,
+            });
+        };
+    });
+    </script>
 </body>
-<!-- script -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
