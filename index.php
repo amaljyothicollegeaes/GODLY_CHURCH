8e76e5e9948278cd93b2858bdb603635f92756b4
ajulkjose246 <mail.ajulkjose@gmail.com>
2023-02-04 22:54:20 +0530

    change template

--- index.php
+++ index.php
@@ -41,12 +41,13 @@
                     <hr class="divider" />
                 </div>
                 <div class="col-lg-8 align-self-baseline">
-                    <h3 class="text-white-75 mb-5">Jesus answered, “I am the way and the truth and the life. No one comes to the Father except through me<br> <b>John 14:6</b></h3>
+                    <h3 class="text-white-75 mb-5">Jesus answered, “I am the way and the truth and the life. No one
+                        comes to the Father except through me<br> <b>John 14:6</b></h3>
                     <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
                 </div>
             </div>
         </div>
-    </header>    
+    </header>
     <!-- Services-->
     <section class="page-section" id="services">
         <div class="container px-4 px-lg-5">
@@ -55,72 +56,80 @@
                     <div class="mt-5">
                         <div class="mb-2"><img class="service_icon" src="./assets/img/live-stream.png"></div>
                         <h3 class="h4 mb-2">Live TV</h3>
-                        <p class="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 text-center services_card">
                     <div class="mt-5">
                         <div class="mb-2"><img class="service_icon" src="./assets/img/holy-chalice.png"></div>
                         <h3 class="h4 mb-2">Holy Mass</h3>
-                        <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 text-center services_card">
                     <div class="mt-5">
                         <div class="mb-2"><img class="service_icon" src="./assets/img/prayer.png"></div>
                         <h3 class="h4 mb-2">Ready to Publish</h3>
-                        <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 text-center services_card">
                     <div class="mt-5">
                         <div class="mb-2"><img class="service_icon" src="./assets/img/give.png"></div>
-                        <h3 class="h4 mb-2">Made with Love</h3>
-                        <p class="text-muted mb-0">Is it really open source if it's not made with love?</p>
+                        <h3 class="h4 mb-2">Offering</h3>
                     </div>
                 </div>
             </div>
         </div>
-    </section>    
+    </section>
     <section>
-    <div id="carouselExampleCaptions" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./assets/img/bg_church.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <img src="./assets/img/holy-bible.jpg" class="d-block w-100" style="height: 300px;">
+                <div class="carousel-item active">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>For I am convinced that neither death nor life, neither angels nor demons, neither the
+                            present nor the future, nor any powers, neither height nor depth, nor anything else in all
+                            creation, will be able to separate us from the love of God that is in Christ Jesus our Lord.
+                        </h5>
+                        <h4>Romans 8:38-39</h4>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>There is no fear in love. But perfect love drives out fear, because fear has to do with punishment. The one who fears is not made perfect in love.
+
+
+                        </h5>
+                        <h4>1 John 4:18</h4>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Jesus looked at them and said, 'With man it is impossible, but not with God. For all things are possible with God.'
+
+
+                        </h5>
+                        <h4>Mark 10:27</h4>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </section>
     <section class="page-section bg-primary" id="about">
         <div class="container px-4 px-lg-5">
